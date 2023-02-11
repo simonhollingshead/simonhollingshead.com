b2b8dfa33b3bea6aa7c882ad0e9f1641f564e9c7
Simon Hollingshead <me@simonhollingshead.com>
2023-02-11 18:52:49 +0000

    Save filesize by removing unused fonts.

---
+++ raw/fas.mjs
@@ -0,0 +1,29 @@
+import { fontawesomeSubset } from "fontawesome-subset";
+fontawesomeSubset({
+    "brands": [
+        "github",
+        "keybase",
+        "linkedin",
+        "mastodon",
+    ],
+    "regular": [],
+    "solid": [
+        "adjust",
+        "bars",
+        "calendar",
+        "circle-exclamation",
+        "circle-xmark",
+        "clock",
+        "envelope",
+        "file-lines",
+        "folder",
+        "lightbulb",
+        "link",
+        "question",
+        "note-sticky",
+        "tag",
+        "triangle-exclamation",
+        "user",
+    ]
+},
+"../themes/hugo-coder/static/fonts/");
